f1d6ddbb7a1d0c03d5008c3e84e47c5898624d4f
Thor <87436831+sanzelda@users.noreply.github.com>
2022-03-27 17:23:08 +0200

    Create templates.py

---
+++ templates.py
@@ -0,0 +1,45 @@
+# this is just to make the code cleaner, so it's all centered here.
+
+prompt_templates = [
+  {
+      "prompt" : "a {adjective} {thing}, {style}",
+  },
+  {
+      "prompt" : "a {color} figure in {location}",
+  },
+  {
+      "prompt": "a {adjective} {thing}, by {artist}, {style}",
+      "weight": 2 
+  },
+  {
+      "prompt": "{divine|demonic} psychedelic artifact of {element}"
+  },
+  {
+      "prompt": "{adjective} ophanim, tarot card, {style}"
+  },
+  {
+      "prompt": "a {adjective} photo of a {location} taken by {artist}, UHD, photorealistic",
+      "weight": 2
+  },
+  {
+      "prompt": "a verdant overgrown {location}, {style}"
+  },
+  {
+      "prompt": "{adjective} {color} {shape}s, vector art by {artist}"
+  },
+  {
+      "prompt": "The {color} of the {thing} is a representation of the Viking's obsession with the {location}"
+  },
+  {
+      "prompt": "The Korean girl is doing a {adjective} {style} painting in the digital age"
+  },
+  {
+      "prompt": "The face of the {animal} is now etched in the {style} art of Japan"
+  },
+  {
+      "prompt": "The Veiled Virgin Statue by {artist} covered in {adjective} cellophane centered in a {location}"
+  },
+  {
+      "prompt": "{adjective} {animal} crystal"
+  }
+]
